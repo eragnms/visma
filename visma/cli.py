d96be205fe8ab90bdbcfca95522011693a8231e7
Mats Gustafsson <mats@mcnab.se>
2024-02-18 10:31:33 +0100

    Improve on ease of login functionality

--- visma/cli.py
+++ visma/cli.py
@@ -4,6 +4,10 @@ import requests
 import datetime
 import json
 from os import environ
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from urllib.parse import urlparse, parse_qs
 
 @click.group()
 def cli():
@@ -49,7 +53,34 @@ def request_access(client, redirect, browser, production):
             f'%20ea:sales%20ea:accounting%20ea:purchase&state=FromPythonCLI&response_type=code'
             f'&prompt=login'
         )
-        __browser.open(sandbox_acc, new=1)
+        # In case the below fetching the code automatically does not work uncomment the below line
+        # __browser.open(sandbox_acc, new=1)
+
+        # Open Firefox browser
+        driver = webdriver.Firefox()
+        # Open the initial page
+        driver.get(sandbox_acc)
+        # Wait for page to load
+        time.sleep(2)  # Adjust this wait time as needed
+        # After the page loads, you can manually enter your credentials in the browser
+        # You can interact with the browser as you would normally do
+        # Wait for the user to manually enter credentials and perform login
+        input("Press Enter after logging in...")
+        # After login, the server will redirect to a new page
+        # Get the current URL
+        new_page_url = driver.current_url
+        print("URL of the new page:", new_page_url)
+        # Close the browser
+        driver.quit()
+        url = new_page_url
+        # Parse the URL
+        parsed_url = urlparse(url)
+        # Extract query parameters
+        query_params = parse_qs(parsed_url.query)
+        # Access the value of the "code" parameter
+        code = query_params.get('code')
+        print("Code:", code[0] if code else "Not found")
+        get_token_non_cli(code[0], client, '', redirect, '', production)
 
 
 @cli.command()
@@ -60,7 +91,10 @@ def request_access(client, redirect, browser, production):
 @click.option('--tokenfile', default='', help='Filename to save token to')
 @click.option('--production', is_flag=True, )
 def get_token(code, client, secret, redirect, tokenfile, production):
+    get_token_non_cli(code, client, secret, redirect, tokenfile, production)
 
+
+def get_token_non_cli(code, client, secret, redirect, tokenfile, production):
     if client == '':
         client = environ.get('VISMA_API_CLIENT_ID')
     if redirect == '':
@@ -92,9 +126,7 @@ def get_token(code, client, secret, redirect, tokenfile, production):
         seconds=(auth_info.get('expires_in', 60)-60))
     auth_info['expires'] = expires.isoformat()
 
-    click.echo(
-        json.dumps(auth_info)
-    )
+    print(json.dumps(auth_info))
 
     with open(tokenfile, 'w') as json_file:
         json.dump(auth_info, json_file, indent=4)
